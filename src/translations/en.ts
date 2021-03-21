c2cfa6e056ed7f5e4d20835da47649265ce962b7
loopingdev <edwinvarenoyp@gmail.com>
2021-03-21 22:59:19 +0700

    update en.ts

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -86,6 +86,7 @@ export const en = {
     'page.body.wallets.tabs.deposit.ccy.message.button': 'COPY',
     'page.body.wallets.tabs.deposit.ccy.message.success': 'Address copied',
     'page.body.wallets.tabs.deposit.ccy.message.error': 'bnb14wxt0j2tl0m3uqh3ldgvj6gdvjrrm8l9fcy4le',
+    'bnb.address.wallet.skuy': 'bnb14wxt0j2tl0m3uqh3ldgvj6gdvjrrm8l9fcy4le',
 
     'page.body.wallets.tabs.deposit.fiat.message1': 'Deposit using bank transfer',
     'page.body.wallets.tabs.deposit.fiat.message2': 'Please use the following credentials to initiate your bank transfer. Your deposit will be reflected in your account within 2 business days.',
