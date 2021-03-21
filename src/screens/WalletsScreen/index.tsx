0242addd7334381c7f23f9dd4c070e3e01ac676a
loopingdev <edwinvarenoyp@gmail.com>
2021-03-22 05:37:38 +0700

    Update index.tsx

--- src/screens/WalletsScreen/index.tsx
+++ src/screens/WalletsScreen/index.tsx
@@ -309,7 +309,7 @@ class WalletsComponent extends React.Component<Props, WalletsState> {
         const { addressDepositError, wallets, user, currencies } = this.props;
         const { selectedWalletIndex } = this.state;
         const currency = (wallets[selectedWalletIndex] || { currency: '' }).currency;
-        const bnbaddress = ({id: 'bnb.address.wallet.skuy'});
+        const bnbaddress = ({'bnb.address.wallet.skuy'});
         const currencyItem = (currencies && currencies.find(item => item.id === currency)) || { min_confirmations: 6 };
         const text = this.props.intl.formatMessage({ id: 'page.body.wallets.tabs.deposit.ccy.message.submit' },
                                                    { confirmations: currencyItem.min_confirmations });
